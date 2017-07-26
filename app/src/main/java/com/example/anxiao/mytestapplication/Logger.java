1ac629aeb6d1131974f11621c2a9c4ebadf1fcef
mart_18502938991 <anmnight@qq.com>
2017-07-26 17:54:54 +0800

    add gaode map

---
+++ app/src/main/java/com/example/anxiao/mytestapplication/Logger.java
@@ -0,0 +1,8 @@
+package com.example.anxiao.mytestapplication;
+
+/**
+ * Created by anxiao on 2017/7/17.
+ */
+
+public class Logger {
+}
---
+++ app/src/main/java/com/example/anxiao/mytestapplication/lesson_android/IntentService.java
@@ -0,0 +1,8 @@
+package com.example.anxiao.mytestapplication.lesson_android;
+
+/**
+ * Created by anxiao on 2017/7/25.
+ */
+
+public class IntentService {
+}
