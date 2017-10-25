590b23a9353c38bb0e18f2c6b1cce2fa39b7833a
mart_18502938991 <anmnight@qq.com>
2017-10-25 17:46:45 +0800

    add web files

---
+++ MyTestProject/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" v-on:click="test()">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  methods:{
+    test:function () {
+      if(window.JavaBridge===undefined||window.JavaBridge===null){
+        alert("device is null")
+      }else {
+        window.JavaBridge.onSynJavaScriptCall("test","test params");
+      }
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
