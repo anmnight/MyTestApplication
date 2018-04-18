81466103dbfaba26431319a28f09426696f61936
zeusqwer <anmnight@qq.com>
2018-04-18 11:09:13 +0800

    change package name

---
+++ app/src/main/java/com/example/testapp/lesson_ui/MomentRefreshLayout.kt
@@ -0,0 +1,12 @@
+package com.example.anxiao.lesson_ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MomentRefreshLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moment_refresh_layout)
+    }
+}
