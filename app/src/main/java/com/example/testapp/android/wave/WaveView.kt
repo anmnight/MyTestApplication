c47f390b39e21008307b9c76b8ad607ea3e48fe0
anmnight@qq.com <an910802>
2018-08-20 21:51:18 +0800

    add wave view

---
+++ app/src/main/java/com/example/testapp/android/wave/WaveView.kt
@@ -0,0 +1,24 @@
+package com.example.testapp.android.wave
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class WaveView @JvmOverloads constructor(context: Context,
+                                         attrs: AttributeSet? = null,
+                                         defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
+
+
+    val mPaint = Paint()
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+
+
+
+
+    }
+}
