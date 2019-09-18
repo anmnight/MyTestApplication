e7175b99b3eed99442ece8d76c43a12bbc5a6c27
anxiao <anmnight@qq.com>
2019-09-18 16:39:06 +0800

    移除image loader module

---
+++ host/src/main/java/com/example/testapp/Tools.kt
@@ -0,0 +1,7 @@
+package com.example.testapp
+
+import android.util.Log
+
+fun debug(message: String) {
+    Log.d("tools", message)
+}
--- host/src/main/java/com/example/testapp/activities/CoordinatorActivity.java
+++ host/src/main/java/com/example/testapp/activities/CoordinatorActivity.java
@@ -1,11 +1,19 @@
 package com.example.testapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
+
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.testapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.testapp.ToolsKt.debug;
+
+
 public class CoordinatorActivity extends AppCompatActivity {
 
     @Override
@@ -13,15 +21,18 @@ public class CoordinatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinator);
 
+
+        Button b = new Button(this);
+
+        List<View> views = new ArrayList<>();
+
+        views.add(b);
+
+        for (View v : views) {
+            debug("id : " + v.getId());
+        }
+
     }
 
-    private ImageView createImageView(Context context, int resId) {
-        ImageView imageView = new ImageView(CoordinatorActivity.this);
-        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        imageView.setLayoutParams(lp);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setImageDrawable(context.getDrawable(resId));
-        return imageView;
-    }
 
 }
