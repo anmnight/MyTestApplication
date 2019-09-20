48d8958496714beee55c820442ec4a58635927a4
anxiao <anmnight@qq.com>
2019-09-20 16:37:10 +0800

    添加AutoLoadImageList

--- host/src/main/java/com/example/testapp/Tools.kt
+++ host/src/main/java/com/example/testapp/Tools.kt
@@ -2,6 +2,34 @@ package com.example.testapp
 
 import android.util.Log
 
+val images = arrayListOf(
+        "https://up.enterdesk.com/edpic_source/53/a6/78/53a678b383be6eb9607e875339b1d052.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s480x854c/g5/M00/00/03/ChMkJ1fJV_yIVgwUAAn8uDtkga0AAU-HQO67rwACfzQ413.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJ1fJV_yIOrfwAAJzkO0xBPQAAU-HQOiUVoAAnOo916.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJlfJV_yIcDniAA9QRHYhtUYAAU-HQOTAP4AD1Bc258.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJlfJV_yIPyQAAACzuvmkILEAAU-HQPyXIgAALPS122.jpg",
+        "https://up.enterdesk.com/edpic_source/53/a6/78/53a678b383be6eb9607e875339b1d052.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s480x854c/g5/M00/00/03/ChMkJ1fJV_yIVgwUAAn8uDtkga0AAU-HQO67rwACfzQ413.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJ1fJV_yIOrfwAAJzkO0xBPQAAU-HQOiUVoAAnOo916.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJlfJV_yIcDniAA9QRHYhtUYAAU-HQOTAP4AD1Bc258.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJlfJV_yIPyQAAACzuvmkILEAAU-HQPyXIgAALPS122.jpg",
+        "https://up.enterdesk.com/edpic_source/53/a6/78/53a678b383be6eb9607e875339b1d052.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s480x854c/g5/M00/00/03/ChMkJ1fJV_yIVgwUAAn8uDtkga0AAU-HQO67rwACfzQ413.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJ1fJV_yIOrfwAAJzkO0xBPQAAU-HQOiUVoAAnOo916.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJlfJV_yIcDniAA9QRHYhtUYAAU-HQOTAP4AD1Bc258.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJlfJV_yIPyQAAACzuvmkILEAAU-HQPyXIgAALPS122.jpg",
+        "https://up.enterdesk.com/edpic_source/53/a6/78/53a678b383be6eb9607e875339b1d052.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s480x854c/g5/M00/00/03/ChMkJ1fJV_yIVgwUAAn8uDtkga0AAU-HQO67rwACfzQ413.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJ1fJV_yIOrfwAAJzkO0xBPQAAU-HQOiUVoAAnOo916.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJlfJV_yIcDniAA9QRHYhtUYAAU-HQOTAP4AD1Bc258.jpg",
+        "http://sjbz.fd.zol-img.com.cn/t_s640x1136c/g5/M00/00/03/ChMkJlfJV_yIPyQAAACzuvmkILEAAU-HQPyXIgAALPS122.jpg"
+)
+
 fun debug(message: String) {
     Log.d("tools", message)
+}
+
+fun dp2px(dp: Int): Int {
+    val density = TestHomeApplication.getInstance().resources.displayMetrics.density
+    return (dp * density).toInt()
 }
---
+++ host/src/main/java/com/example/testapp/activities/AutoLoadImageList.kt
@@ -0,0 +1,71 @@
+package com.example.testapp.activities
+
+import android.graphics.Canvas
+import android.graphics.Rect
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.anmnight.imageloader.AutoLoadImage
+import com.example.testapp.R
+import com.example.testapp.dp2px
+import com.example.testapp.images
+import kotlinx.android.synthetic.main.activity_auto_load_image_list.*
+
+class AutoLoadImageList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auto_load_image_list)
+
+        val manager = LinearLayoutManager(this)
+        image_list.layoutManager = manager
+        image_list.adapter = ImageAdapter()
+        image_list.addItemDecoration(ImageDecoration())
+
+    }
+
+
+    inner class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(this@AutoLoadImageList).inflate(R.layout.list_image_simple_item, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return images.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.imageView.displayImage(images[position])
+        }
+
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val imageView: AutoLoadImage = itemView.findViewById(R.id.image_item)
+        }
+    }
+
+
+    inner class ImageDecoration : RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            super.getItemOffsets(outRect, view, parent, state)
+            outRect.top = dp2px(2)
+            outRect.right = dp2px(2)
+            outRect.left = dp2px(2)
+        }
+
+        override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+            super.onDraw(c, parent, state)
+            val childView = parent.getChildAt(2).findViewById<AutoLoadImage>(R.id.image_item)
+            parent.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+                childView.scrollBy(0, 5)
+            }
+        }
+    }
+}
